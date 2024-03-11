25461ec41592f80cc4d069a387ab6859812ba248
KirillSmirnov <smirnowku@gmail.com>
2024-03-11 13:10:52 +0300

    Large message encryption test

--- crypto-messenger-coder/src/test/java/cryptomessenger/coder/CoderTest.java
+++ crypto-messenger-coder/src/test/java/cryptomessenger/coder/CoderTest.java
@@ -1,11 +1,14 @@
 package cryptomessenger.coder;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -60,6 +63,24 @@ public class CoderTest {
                 CoderException.class,
                 () -> coder.decode(encodedMessage, keyPair.getPrivateKey())
         );
+    }
+
+    @Test
+    @Disabled("Not implemented")
+    public void givenLargeMessage_whenEncodeAndDecode_thenSuccess() {
+        var keyPair = coder.generateKeyPair();
+        var originalMessage = getLargeMessage();
+
+        var encodedMessage = coder.encode(originalMessage, keyPair.getPublicKey());
+        var decodedMessage = coder.decode(encodedMessage, keyPair.getPrivateKey());
+
+        assertThat(decodedMessage.getBytes()).isEqualTo(originalMessage.getBytes());
+    }
+
+    private Message getLargeMessage() {
+        var bytes = new byte[10_000];
+        ThreadLocalRandom.current().nextBytes(bytes);
+        return () -> bytes;
     }
 
     @TestConfiguration
